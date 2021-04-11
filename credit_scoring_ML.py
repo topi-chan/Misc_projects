8dc650c7cc21c2d3aa052c002b40e3cb0e07e4b5
Maciej Topolewski <maciej@top-project.com.pl>
2021-04-11 15:01:19 +0200

    more data cleaning

--- credit_scoring_ML.py
+++ credit_scoring_ML.py
@@ -2,11 +2,34 @@ import pandas as pd
 import numpy as np
 import matplotlib
 import sklearn
+import seaborn as sns
 
-"..."
 
 df = pd.read_csv('Loan_data.csv')
-
+empty_col = []
 for col in df.columns:
     pct_missing = np.mean(df[col].isnull())
     print('{} - {}%'.format(col, round(pct_missing*100)))
+    if pct_missing == 1:
+        empty_col.append(col)
+print(empty_col)
+df.head()
+
+
+
+cols = df.columns[:30]
+colours = ['#000099', '#ffff00'] # yellow is missing. blue is not missing.
+sns.heatmap(df[cols].isnull(), cmap=sns.color_palette(colours))
+print(empty_col)
+df_minus_empty_col = df.drop(empty_col, axis=1)
+
+print(df_minus_empty_col.head(n=20))
+
+almost_empty_col = []
+for col in df_minus_empty_col.columns:
+    pct_missing = np.mean(df[col].isnull())
+    value = float(np.mean(df[col].isnull()))
+    print('{} - {}%'.format(col, round(pct_missing*100)))
+    if round(pct_missing) == 1:
+        almost_empty_col.append(col), almost_empty_col.append(value)
+print(almost_empty_col)
