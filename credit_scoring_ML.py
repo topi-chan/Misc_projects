9e1dffbb66fefd40e67fcbebd8a6a6f45c0e6e82
Maciej Topolewski <73426777+topi-chan@users.noreply.github.com>
2021-04-25 17:18:16 +0200

    Model updated - data processing

--- credit_scoring_ML.py
+++ credit_scoring_ML.py
@@ -16,9 +16,8 @@ for col in df.columns:
 print(empty_col)
 df.head()
 
-
 cols = df.columns[:30]
-colours = ['#000099', '#ffff00'] # yellow is missing. blue is not missing.
+colours = ['#000099', '#ffff00'] # yellow is missing. blue is not missing
 sns.heatmap(df[cols].isnull(), cmap=sns.color_palette(colours))
 print(empty_col)
 df_minus_empty_col = df.drop(empty_col, axis=1)
@@ -33,15 +32,46 @@ for col in df_minus_empty_col.columns:
     if round(pct_missing) == 1:
         almost_empty_col.append(col), almost_empty_col.append(value)
 print(almost_empty_col)
+df = df_minus_empty_col
 
-with open('LCDataDictionary.csv', newline='') as csvfile:
-    read_data_dict = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in read_data_dict:
-        print(', '.join(row))
+df_descr = pd.read_csv('LCDataDictionary.csv')
+df_descr[df_descr.columns[:2]].head(n=10)
+df_descr.drop(df_descr.columns[2:],axis=1,inplace=True)
+df_descr.head()
+df_descr.iloc[:1]
 
-data_dict = pd.read_csv('LCDataDictionary.csv')
-print(data_dict.head(n=20))
-info_dict = {}
-for index, row in data_dict.iterrows():
-    if row.any() in almost_empty_col:
-        print('x', row)
+for col in df_descr.columns:
+    print(col)
+loc = []
+for element in almost_empty_col:
+    loc.append(df_descr.index[df_descr['LoanStatNew']==element].tolist())
+for l in loc:
+    print(df_descr.loc[l, 'Description'])
+
+df_descr.loc[50, 'Description']
+df_descr.loc[52, 'Description']
+df_descr.loc[58, 'Description']
+df_descr.loc[145, 'Description']
+df_descr.loc[146, 'Description']
+df_descr.loc[147, 'Description']
+df_descr.loc[148, 'Description']
+df_descr.loc[149, 'Description']
+df_descr.loc[150, 'Description']
+
+for l in loc:
+    print(df_descr.loc[l, 'LoanStatNew'])
+df['mths_since_last_delinq'].head()
+
+df['mths_since_last_delinq'] = df['mths_since_last_delinq'].replace(to_replace
+                                                    = np.nan, value = -99999)
+df['mths_since_last_delinq'].head()
+
+df['mths_since_last_record'].head()
+df['mths_since_last_record'] = df['mths_since_last_record'].replace(to_replace
+                                                    = np.nan, value = -99999)
+df['mths_since_last_record'].head()
+
+
+#df.iloc[:,50].fillna(0, inplace=True)
+
+#df_empty = pd.DataFrame({'A' : []})
