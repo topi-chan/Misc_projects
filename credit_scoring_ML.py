79611d90d6ed7a67e7ea592075fb18d17297636e
Maciej Topolewski <73426777+topi-chan@users.noreply.github.com>
2021-04-27 20:00:55 +0200

    Finished data processing

--- credit_scoring_ML.py
+++ credit_scoring_ML.py
@@ -2,7 +2,12 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 from sklearn import preprocessing
-import matplotlib, sklearn, csv, random, statistics, re
+import matplotlib
+import sklearn
+import csv
+import random
+import statistics
+import re
 
 
 df = pd.read_csv('Loan_data.csv')
@@ -16,12 +21,13 @@ print(empty_col)
 df.head()
 
 cols = df.columns[:]
-colours = ['#000099', '#ffff00'] # yellow is missing. blue is not missing
+colours = ['#000099', '#ffff00']  # yellow is missing. blue is not missing
 sns.heatmap(df[cols].isnull(), cmap=sns.color_palette(colours))
 print(empty_col)
 df_minus_empty_col = df.drop(empty_col, axis=1)
 
 print(df_minus_empty_col.head(n=20))
+
 
 def empty_check(df_instance):
     almost_empty_col = []
@@ -32,24 +38,28 @@ def empty_check(df_instance):
         if round(pct_missing) == 1:
             almost_empty_col.append(col), almost_empty_col.append(value)
     print(almost_empty_col)
+
+
 empty_check(df_minus_empty_col)
 df = df_minus_empty_col
 
 df_descr = pd.read_csv('LCDataDictionary.csv')
 df_descr[df_descr.columns[:2]].head(n=10)
-df_descr.drop(df_descr.columns[2:],axis=1,inplace=True)
+df_descr.drop(df_descr.columns[2:], axis=1, inplace=True)
 df_descr.head()
 df_descr.iloc[:1]
 
 for col in df_descr.columns:
     print(col)
 
+
 def print_description(col_name_list):
     loc = []
     for element in col_name_list:
-        loc.append(df_descr.index[df_descr['LoanStatNew']==element].tolist())
+        loc.append(df_descr.index[df_descr['LoanStatNew'] == element].tolist())
     for l in loc:
         print(df_descr.loc[l, 'Description'])
+
 
 print_description(almost_empty_col)
 
@@ -70,12 +80,11 @@ df['mths_since_last_delinq'].head()
 df['mths_since_last_delinq'].mean()
 df['mths_since_last_delinq'].median()
 df['mths_since_last_delinq'].max()
-df['mths_since_last_delinq'].quantile([0.25,0.5,0.75])
+df['mths_since_last_delinq'].quantile([0.25, 0.5, 0.75])
 df['mths_since_last_delinq'] = pd.qcut(df
-                              ['mths_since_last_delinq'].values, q = 10).codes+1
+                                       ['mths_since_last_delinq'].values, q=10).codes+1
 
-df['mths_since_last_delinq'] = df['mths_since_last_delinq'].replace(to_replace
-                                                            = 0, value = 20)
+df['mths_since_last_delinq'] = df['mths_since_last_delinq'].replace(to_replace=0, value=20)
 df['mths_since_last_delinq'].head(n=25)
 
 
@@ -83,21 +92,20 @@ df['mths_since_last_record'].head()
 df['mths_since_last_record'].mean()
 df['mths_since_last_record'].median()
 df['mths_since_last_record'].max()
-df['mths_since_last_record'].quantile([0.25,0.5,0.75])
+df['mths_since_last_record'].quantile([0.25, 0.5, 0.75])
 df['mths_since_last_record'] = pd.qcut(df['mths_since_last_record'].values,
-                               q = 10, duplicates='drop').codes+1
-df['mths_since_last_record'] = df['mths_since_last_record'].replace(to_replace
-                                                            = 0, value = 20)
+                                       q=10, duplicates='drop').codes+1
+df['mths_since_last_record'] = df['mths_since_last_record'].replace(to_replace=0, value=20)
 df['mths_since_last_record'].mean()
 
 
 df = df.drop(['next_pymnt_d', 'debt_settlement_flag_date', 'settlement_status',
-        'debt_settlement_flag_date', 'settlement_date', 'settlement_amount',
-        'settlement_percentage', 'settlement_term'], axis = 1)
+              'debt_settlement_flag_date', 'settlement_date', 'settlement_amount',
+              'settlement_percentage', 'settlement_term'], axis=1)
 
 
 cols = df.columns[:30]
-colours = ['#000099', '#ffff00'] # yellow is missing. blue is not missing
+colours = ['#000099', '#ffff00']  # yellow is missing. blue is not missing
 sns.heatmap(df[cols].isnull(), cmap=sns.color_palette(colours))
 
 df['desc'].head()
@@ -135,12 +143,12 @@ for i, row in df['desc'].iteritems():
         row_value = 0.75
     else:
         row_value = 1
-    df.at[i,'desc'] = row_value
+    df.at[i, 'desc'] = row_value
 
 df['desc'].head(n=15)
 
 cols = df.columns[29:]
-colours = ['#000099', '#ffff00'] # yellow is missing. blue is not missing
+colours = ['#000099', '#ffff00']  # yellow is missing. blue is not missing
 sns.heatmap(df[cols].isnull(), cmap=sns.color_palette(colours))
 
 empty_check(df)
@@ -163,11 +171,11 @@ for i, row in df['emp_length'].iteritems():
         emp_lenght_value = int(re_list.pop(0))
     else:
         emp_lenght_value = 0
-    df.at[i,'emp_length'] = emp_lenght_value
+    df.at[i, 'emp_length'] = emp_lenght_value
 
 df['emp_length'].head(n=25)
 
-df = df.drop(['emp_title'], axis = 1)
+df = df.drop(['emp_title'], axis=1)
 
 
 empty_check(df)
@@ -175,15 +183,14 @@ empty_check(df)
 df.loc[random.randrange(1, 40520), 'pub_rec_bankruptcies']
 print_description(['pub_rec_bankruptcies'])
 df['pub_rec_bankruptcies'] = df['pub_rec_bankruptcies'].fillna(
-                             df['pub_rec_bankruptcies'].mean())
+    df['pub_rec_bankruptcies'].mean())
 
 empty_check(df)
 
 df['pub_rec_bankruptcies'].head(n=15)
 
 
-
-#check if dataframe contains only numeric values
+# check if dataframe contains only numeric values
 def numeric_check():
     str_elem = df.apply(lambda s: pd.to_numeric(s, errors='coerce').notnull().all())
     only_numeric_elements = []
@@ -192,13 +199,15 @@ def numeric_check():
         if row == 'False':
             only_numeric_elements.append(i)
     print(only_numeric_elements)
+
+
 numeric_check()
 
 df['id'].head(n=25)
 
 df.applymap(lambda x: isinstance(x, (int, float)))
 
-#these for which to_numeric returned true
+# these for which to_numeric returned true
 df['id']
 df['desc']
 df['mths_since_last_delinq']
@@ -217,12 +226,14 @@ df = df.drop(39786)
 
 numeric_check()
 
+
 def check_non_num_rows(col_name):
     for i, row in df[col_name].iteritems():
         try:
             row = int(row)
         except:
             print(i, row)
+
 
 check_non_num_rows('term')
 
@@ -231,110 +242,114 @@ def check_consistency(column, *phrases):
     for i, row in df[column].iteritems():
         row = str(row)
         found = False
-        for r in range (len(phrases)):
+        for r in range(len(phrases)):
             if re.search(phrases[r], row):
                 found = True
         if found != True:
             print(i, row)
 
 
-#change months into integers
+# change months into integers
 for i, row in df['term'].iteritems():
     row = str(row)
     if re.search("36 months", row):
         term = 0
     elif re.search("60 months", row):
         term = 1
-    df.at[i,'term'] = term
-#0 = 36 months; 1 = 60 months; no other value is present
+    df.at[i, 'term'] = term
+# 0 = 36 months; 1 = 60 months; no other value is present
 
 check_non_num_rows('int_rate')
-#change % into floats
+# change % into floats
 df['int_rate'].dtypes
 # for i, row in df['term'].iteritems():
 #     row = float(row)
 #     print(row)
-#maybe this will suffice? as data is hardly converted here
+# maybe this will suffice? as data is hardly converted here
 
 
 check_non_num_rows('grade')
-#possibly leave it like that or change into integers for coherency and speed
-#let's use hot encoding.
+# possibly leave it like that or change into integers for coherency and speed
+# let's use hot encoding.
+
 
 def hot_encoding(col):
     global df
     df = pd.concat([df.drop(col, axis=1), pd.get_dummies(df[col])], axis=1)
     return df
+
+
 df = hot_encoding('grade')
 df.head()
 
 check_non_num_rows('sub_grade')
-#as above
+# as above
 df = hot_encoding('sub_grade')
 df.head()
 
 check_non_num_rows('home_ownership')
-#as above + check if there is any abbreviation from the rule
-#MORTGAGE OWN RENT
+# as above + check if there is any abbreviation from the rule
+# MORTGAGE OWN RENT
 check_consistency('home_ownership', 'MORTGAGE', 'OWN', 'RENT')
 
-df.at[39786,'home_ownership'] = 'NONE'
+df.at[39786, 'home_ownership'] = 'NONE'
 df = hot_encoding('home_ownership')
 
 
 check_non_num_rows('annual_inc')
-#set empty rows to mean or median or 0 or drop them
+# set empty rows to mean or median or 0 or drop them
 # df['annual_inc'].head(n=25)
 # for i, row in df['annual_inc'].iteritems():
 #     if row == 0.00:
 #         print(i, row)
 # df['annual_inc'].replace(to_replace=np.nan, value=0)
-#dropping is probably a better idea, since nan may be missing value and may
-#cloud a model
+# dropping is probably a better idea, since nan may be missing value and may
+# cloud a model
 df = df.drop(labels=[39786, 42450, 42451, 42481, 42534])
 
 
 check_non_num_rows('verification_status')
-#check whether there are only 3 values and change them into integers
+# check whether there are only 3 values and change them into integers
 check_consistency('verification_status', 'Not Verified', 'Verified', 'Source Verified')
-#ok!
+# ok!
 check_non_num_rows('issue_d')
-#change into dates / numbers
+# change into dates / numbers
 df['issue_d'] = pd.to_datetime(df['issue_d'])
 df['issue_d'] = df['issue_d'].dt.date
-#changed into date, can it be properly understood by the model?
+# changed into date, can it be properly understood by the model?
 
 check_non_num_rows('loan_status')
-#check whether there are only 2 values and change them into integers
+# check whether there are only 2 values and change them into integers
 #!! this is value to be predicted by the model!!
 check_consistency('loan_status', 'Fully Paid',  'Charged Off')
 df = df.drop(labels=39786)
-#dropped one empty row - useless for the model
+# dropped one empty row - useless for the model
 
 
 check_non_num_rows('pymnt_plan')
-#check if there is other value than "N" and if not - drop column
+# check if there is other value than "N" and if not - drop column
 check_consistency('pymnt_plan', 'n')
-df = df.drop(labels = 'pymnt_plan', axis = 1)
+df = df.drop(labels='pymnt_plan', axis=1)
 
 check_non_num_rows('url')
-#drop, seems irrelevant; also: url-s needs login, don't know if working
-df = df.drop(labels = 'url', axis = 1)
+# drop, seems irrelevant; also: url-s needs login, don't know if working
+df = df.drop(labels='url', axis=1)
 
 check_non_num_rows('purpose')
-#assign randomly generated numbers for 'purpose' items or clean and hot encoding
+# assign randomly generated numbers for 'purpose' items or clean and hot encoding
 row_list = []
 for i, row in df['purpose'].iteritems():
     row_list.append(row)
 list(set(row_list))
 df['purpose'].fillna("other", inplace=True)
 check_consistency('purpose', 'major_purchase', 'home_improvement',
-'debt_consolidation', 'wedding', 'small_business', 'house', 'renewable_energy',
-'car','credit_card', 'medical', 'educational', 'moving', 'other', 'vacation')
+                  'debt_consolidation', 'wedding', 'small_business', 'house', 'renewable_energy',
+                  'car', 'credit_card', 'medical', 'educational', 'moving', 'other', 'vacation')
 df = hot_encoding('purpose')
 
 check_non_num_rows('title')
-#drop?
+# drop?
+
 
 def generate_checklist(col):
     row_list = []
@@ -343,111 +358,143 @@ def generate_checklist(col):
     print(list(set(row_list)))
     print(len(list(set(row_list))))
 
+
 generate_checklist('title')
-#too many unrelated variables - drop
-df = df.drop(labels = 'title', axis = 1)
+# too many unrelated variables - drop
+df = df.drop(labels='title', axis=1)
 
 check_non_num_rows('zip_code')
-#drop the 'xx'? check if it's relevant at all
+# drop the 'xx'? check if it's relevant at all
 generate_checklist('zip_code')
-#dunno what to do with  it. 838 values but seems relevant.
-#choosen - label encoding, as number of variables is substantial.
+# dunno what to do with  it. 838 values but seems relevant.
+# choosen - label encoding, as number of variables is substantial.
 df['zip_code'] = preprocessing.LabelEncoder().fit_transform(df['zip_code'])
 df.head()
 
 check_non_num_rows('addr_state')
-#assign into numbers
+# assign into numbers
 generate_checklist('addr_state')
-#label encoding or hot encoding?
-## TODO: choose encoding
+# label encoding or hot encoding?
+# TODO: choose encoding
 
 check_non_num_rows('delinq_2yrs')
 df['delinq_2yrs'].head(n=35)
-#replace nan with median() value
+# replace nan with median() value
 df['delinq_2yrs'].fillna(df['delinq_2yrs'].median(), inplace=True)
 
 
 check_non_num_rows('earliest_cr_line')
-#change into dates
+# change into dates
 df['earliest_cr_line'] = pd.to_datetime(df['earliest_cr_line'])
 df['earliest_cr_line'] = df['earliest_cr_line'].dt.date
 
 check_non_num_rows('inq_last_6mths')
-#it is important value; also there is a pattern - on last rows severas columns
-#are missing - let's drop them for consistency (but check other columns first)
+# it is important value; also there is a pattern - on last rows severas columns
+# are missing - let's drop them for consistency (but check other columns first)
 df.loc[42525]
 df.loc[42526]
 df = df.drop(df.index[42515:])
 
 check_non_num_rows('open_acc')
-#replace nan with mean() value or drop last columns - repeating 'nan' pattern
+# replace nan with mean() value or drop last columns - repeating 'nan' pattern
 df['open_acc'].mean()
 df['open_acc'].median()
 df['open_acc'].std()
 df['open_acc'].fillna(df['open_acc'].median(), inplace=True)
 
 check_non_num_rows('pub_rec')
-#as above
-#repeating pattern of missing values for some records, eg. 42450 - drop.
+# as above
+# repeating pattern of missing values for some records, eg. 42450 - drop.
 df.drop(labels=[39786, 42450, 42451, 42460, 42473, 42481, 42484, 42495, 42510],
-                inplace=True)
+        inplace=True)
 
 check_non_num_rows('revol_util')
-#change % into floats
-## TODO: check for 'int_rate' and apply same rules
+# change % into floats
+# TODO: check for 'int_rate' and apply same rules
 
 check_non_num_rows('total_acc')
-#replace nan with mean() value or drop last columns - repeating 'nan' pattern
+# replace nan with mean() value or drop last columns - repeating 'nan' pattern
+# update: already dropped in previous analysis
 
 check_non_num_rows('initial_list_status')
-#investigate this column and its meaning
+# investigate this column and its meaning
+for i, row in df['initial_list_status'].iteritems():
+    row = str(row)
+    if row == "w":
+        print(row)
+    elif row != "f":
+        print("Found something else!")
+df['initial_list_status'].value_counts()
+# column is "broken" - contains only 'f' values, hence is irrelevant
+df.drop(columns='initial_list_status', inplace=True)
+
 check_non_num_rows('last_pymnt_d')
-#change into dates / numbers?
+# change into dates / numbers?
+df['last_pymnt_d'] = pd.to_datetime(df['last_pymnt_d'])
+df['last_pymnt_d'] = df['last_pymnt_d'].dt.date
+
 check_non_num_rows('last_credit_pull_d')
-#as above
+# as above
+df['last_credit_pull_d'] = pd.to_datetime(df['last_credit_pull_d'])
+df['last_credit_pull_d'] = df['last_credit_pull_d'].dt.date
+
 check_non_num_rows('collections_12_mths_ex_med')
-#replace nan with mean() value or drop last columns - repeating 'nan' pattern
+# replace nan with mean() value or drop last columns - repeating 'nan' pattern
+df['collections_12_mths_ex_med'].mean()
+df['collections_12_mths_ex_med'].median()
+df['collections_12_mths_ex_med'].std()
+df['collections_12_mths_ex_med'].sum()
+df['collections_12_mths_ex_med'] #empty column - drop
+df.drop(columns='collections_12_mths_ex_med', inplace=True)
+
 check_non_num_rows('application_type')
-#check if there is other than "individual" and if not - drop column
+# check if there is other than "individual" and if not - drop column
+generate_checklist('application_type')
+df.drop(columns='application_type', inplace=True) #no other value - drop
+
 check_non_num_rows('acc_now_delinq')
-#replace nan with mean() value or drop last columns - repeating 'nan' pattern
+# replace nan with mean() value or drop last columns - repeating 'nan' pattern
+#update: already solved by dropping above columns
+
 check_non_num_rows('chargeoff_within_12_mths')
-#replace nan with mean() value or drop last columns - repeating 'nan' pattern
-#or maybe a median() value
+# replace nan with mean() value or drop last columns - repeating 'nan' pattern
+# or maybe a median() value
+df['chargeoff_within_12_mths'].mean()
+df['chargeoff_within_12_mths'].median()
+df['chargeoff_within_12_mths'].std()
+df['chargeoff_within_12_mths'] #either null or nan - drop
+df.drop(columns='chargeoff_within_12_mths', inplace=True)
+
 check_non_num_rows('delinq_amnt')
-#replace nan with mean() value or drop last columns - repeating 'nan' pattern
+# replace nan with mean() value or drop last columns - repeating 'nan' pattern
+#update: already solved by dropping above columns
+
 check_non_num_rows('tax_liens')
-#replace nan with mean() value or drop last columns - repeating 'nan' pattern
+# replace nan with mean() value or drop last columns - repeating 'nan' pattern
+df['tax_liens'].mean()
+df['tax_liens'].median()
+df['tax_liens'].std()
+#it seems like an important feature - keep the column, fix nan's
+df['tax_liens'].head(n=45)
+generate_checklist('tax_liens')
+check_consistency('tax_liens', '0.0', '1.0')
+df['tax_liens'].value_counts()
+#this column will be irrelevant for the model - single value '1' will be either
+#in test or train data; though for larger database it could matter
+df.drop(columns='tax_liens', inplace=True)
+
 check_non_num_rows('hardship_flag')
-#check if there is other than "N" and if not - drop column
+# check if there is other than "N" and if not - drop column
+df['hardship_flag'].value_counts()
+df.drop(columns='hardship_flag', inplace=True)#single value present in column
+
 check_non_num_rows('disbursement_method')
-#check if there is other than "cash" and if not - drop column
+# check if there is other than "cash" and if not - drop column
+df['disbursement_method'].value_counts()#single value present in column
+df.drop(columns='disbursement_method', inplace=True)
+
 check_non_num_rows('debt_settlement_flag')
-#check if there is other than "cash" and if not - drop column
-
-
-
-
-
-
-
-
-
-df['funded_amnt']
-df['term']
-str_elem.loc[str_elem[1] == 'False']
-
-str_elem(df.iloc[:,1])
-index = str_list.index(['False'])
-
-for col in df.columns:
-    for i, row in df[col].iteritems():
-        row.to_numeric()
-        # try:
-        #     row.to_numeric()
-        # except:
-        #     Exception('Błąd!')
-
-#df.iloc[:,50].fillna(0, inplace=True)
-
-#df_empty = pd.DataFrame({'A' : []})
+# check if there is other than "cash" and if not - drop column
+df['debt_settlement_flag'].value_counts()
+df['debt_settlement_flag'].replace('Y', 1)
+df['debt_settlement_flag'].replace('N', 0)
