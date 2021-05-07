e8ccc797d1c56b4b5f2e17dcc31e907c40d1e09d
Maciej Topolewski <73426777+topi-chan@users.noreply.github.com>
2021-05-07 21:44:49 +0200

    separated preprocessing into different file

--- credit_scoring_ML_model.py
+++ credit_scoring_ML_model.py
@@ -7,17 +7,6 @@ data = pd.read_csv('credit_scoring_for_model.csv')
 for col in data.columns:
     print(col)
 
-data.applymap(np.isreal)
-
-only_num_series = data.select_dtypes(include=np.number).columns.tolist()
-all_series = []
-for col in data.columns:
-    all_series.append(col)
-non_num_series = set(only_num_series) ^ set(all_series)
-non_num_series
-data['earliest_cr_line'].value_counts()
-
-data.drop(columns='int_rate', inplace=True)
 
 y = data['loan_status'].values
 
